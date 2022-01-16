19719dae3fb523176d53964cba3bc3b449a70017
taylordunn <t.dunn19@gmail.com>
2022-01-16 16:17:48 -0400

    Added more functions to read/write data

--- R/fct_data.R
+++ R/fct_data.R
@@ -10,3 +10,65 @@ register_github_board <- function() {
     name = "github", repo = "taylordunn/canadacovidshiny", path = "data/pins"
   )
 }
+
+#' The list of two-letter province codes
+#'
+#' @noRd
+province_codes <- list(
+  "Alberta" = "AB", "British Columbia" = "BC", "Manitoba" = "MB",
+  "New Brunswick" = "NB", "Newfoundland and Labrador" = "NL",
+  "Northwest Territories" = "NT", "Nova Scotia" = "NS",
+  "Nunavut" = "NU", "Ontario" = "ON", "Prince Edward Island" = "PE",
+  "Quebec" = "QC", "Saskatchewan" = "SK", "Yukon" = "YT"
+)
+
+#' Reads in the summary data from pins board
+#'
+#' @param split One of "overall" (aggregated counts across Canada) or "province"
+#'   (splits counts by province/territory).
+#'
+#' @return A data frame.
+#'
+#' @importFrom pins pin_get
+read_summary <- function(split = c("overall", "province")) {
+  split <- match.arg(split)
+  pins::pin_get(paste0("summary_", split), board = "github")
+}
+
+#' Reads in the reports data from pins board
+#'
+#' @param choice if `NULL`, reads in all of the reports and returns a list.
+#'   Otherwise, one of "overall" or a province code ("AB", "BC", etc.).
+#'
+#' @return A single data frame, or a named list of data frames (if `choice`
+#'   is `NULL`).
+#'
+#' @importFrom purrr map
+#' @importFrom pins pin_get
+read_reports <- function(choice = NULL) {
+  # The possible choices are "overall" or lowercase province codes
+  choices <- c("overall", tolower(province_codes))
+
+  if (is.null(choice)) {
+    purrr::map(
+      choices,
+      ~ pins::pin_get(paste0("reports_", .x), board = "github")
+    ) %>%
+      setNames(choices)
+  } else {
+    choice <- match.arg(tolower(choice), choices = choices)
+
+    pins::pin_get(paste0("reports_", choice), board = "github")
+
+  }
+}
+
+#' Reads in the reports data from pins board
+#'
+#' @param data The data to save to the pin board.
+#' @param name The name to give the data.
+#'
+#' @importFrom pins pin
+write_data <- function(data, name) {
+  pins::pin(data, name = name, board = "github")
+}
--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,9 +1,32 @@
 test_that("register_github_board works", {
   board <- register_github_board()
-  expect_cl
   expect_s3_class(board, "pins_board_github")
 
   summary_overall_info <- pins::pin_info("summary_overall", "github")
   expect_equal(summary_overall_info$cols, 22)
   expect_equal(summary_overall_info$rows, 1)
+})
+
+test_that("read_summary works", {
+  board <- register_github_board()
+
+  summary_overall <- read_summary("overall")
+  expect_equal(nrow(summary_overall), 1)
+  expect_equal(ncol(summary_overall), 22)
+
+  summary_province <- read_summary("province")
+  expect_equal(nrow(summary_province), 13)
+  expect_equal(ncol(summary_province), 23)
+})
+
+test_that("read_reports works", {
+  board <- register_github_board()
+
+  reports <- read_reports()
+  expect_equal(names(reports), c("overall", tolower(province_codes)))
+
+  reports_ns <- read_reports(choice = "NS")
+  expect_equal(unique(reports_ns$province), "NS")
+  expect_equal(ncol(reports_ns), 23)
+
 })
